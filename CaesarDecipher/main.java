1eb32c01feb2fc23381bf3896c47b80877a7408a
DaleNaci <dale.nash01@gmail.com>
2018-07-06 02:57:48 -0400

    Finish a rough Caesar Decipher, still up for a  lot of change

--- CaesarDecipher/main.java
+++ CaesarDecipher/main.java
@@ -3,19 +3,18 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 
-
 class main {
 
-    // Variable Declaration
-    String encoded_message;
+    static boolean valid;
+    static String encoded;
+    static String adjusted;
 
     public static boolean check_for_word(String word) {
         try {
             BufferedReader in = new BufferedReader(new FileReader("words.txt"));
-            System.out.println("hello");
             String str;
             while ((str = in.readLine()) != null) {
-                if (str.indexOf(word) != -1) {
+                if (str.indexOf(word) == 0) {
                     return true;
                 }
             }
@@ -26,26 +25,46 @@ class main {
         return false;
     }
 
-    public static boolean prompt() {
+    public static void prompt() {
         System.out.println("Enter encoded message: ");
         Scanner sc = new Scanner(System.in);
-        encoded_message = sc.nextLine();
-        if (encoded_message < 2) {
+        encoded = sc.nextLine();
+        if (encoded.length() < 2) {
             System.out.println("Too short");
             prompt();
         }
+        encoded = encoded.toLowerCase();
     }
+
+    public static void shift_message() {
+        for (int i = 0; i < adjusted.length(); i++) {
+            char c = adjusted.charAt(i);
+            if (96 < c && c < 123) {
+                if (c == 'z')
+                    c-= 25;
+                else
+                    c++;
+            }
+            adjusted = adjusted.substring(0, i) + c + adjusted.substring(i+1, adjusted.length());
+        }
+    }
+
 
     public static void main(String[] args) {
         prompt();
-        
 
+        valid = false;
+        adjusted = encoded;
+        shift_message();
+
+        for (int i=0; i<25; i++) {
+            for (int j=3; j<=encoded.length(); j++) {
+                if (check_for_word(adjusted.substring(0, j))) {
+                    System.out.println(adjusted);
+                    j=encoded.length();
+                }
+            }
+            shift_message();
+        }
     }
 }
-
-
-
-
-
-
-
