a821da0a832cd25316b2d1c8c09e0a7fa9370c89
DaleNaci <dale.nash01@gmail.com>
2019-11-05 16:31:17 -0500

    Going to Terminal and hitting Enter terminates the mouse movement now

--- RegistrationReloader/reload.py
+++ RegistrationReloader/reload.py
@@ -1,15 +1,25 @@
 from pynput.mouse import Button, Controller
-from pynput.keyboard import Key, Listener
+import threading as th
 import time
 
 mouse = Controller()
 
+keep_going = True
+def key_capture_thread():
+    global keep_going
+    input()
+    keep_going = False
+
 time.sleep(2)
 
-while True:
-    mouse.position = (84.9, 320.2)
-    mouse.click(Button.left, 1)
-    time.sleep(.1)
-    mouse.position = (1392.2, 206.5)
-    mouse.click(Button.left, 1)
-    time.sleep(.1)
+def do_stuff():
+    th.Thread(target=key_capture_thread, args=(), name='key_capture_thread', daemon=True).start()
+    while keep_going:
+        mouse.position = (84.9, 320.2)
+        mouse.click(Button.left, 1)
+        time.sleep(.1)
+        mouse.position = (1392.2, 206.5)
+        mouse.click(Button.left, 1)
+        time.sleep(.1)
+
+do_stuff()
